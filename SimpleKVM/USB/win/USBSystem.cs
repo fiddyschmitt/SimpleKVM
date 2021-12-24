14395b92b25d51a6408e7c13f82ccd2d8977da75
fiddyschmitt <fiddyschmitt@gmail.com>
2021-12-24 14:16:55 +1000

    Detect more USB classes to diagnose issue with detecting Microsoft Wireless Notebook Optical Mouse 3000 dongle

--- SimpleKVM/USB/win/USBSystem.cs
+++ SimpleKVM/USB/win/USBSystem.cs
@@ -11,25 +11,37 @@ namespace SimpleKVM.USB.win
     {
         public USBSystem()
         {
-            var insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            WatchDeviceClass("Win32_USBHub");
+
+            WatchDeviceClass("Win32_USBController");
+            //WatchDeviceClass("Win32_USBControllerDevice");
+            WatchDeviceClass("Win32_Keyboard");
+            WatchDeviceClass("Win32_PointingDevice");
+            //WatchDeviceClass("Win32_PnPDevice");
+            WatchDeviceClass("Win32_PnPEntity");
+        }
+
+        void WatchDeviceClass(string deviceClass)
+        {
+            var insertQuery = new WqlEventQuery($"SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA '{deviceClass}'");
             var insertWatcher = new ManagementEventWatcher(insertQuery);
-            insertWatcher.EventArrived += (sender, e) => PropogateEvent(e, EnumUsbEvent.Inserted);
+            insertWatcher.EventArrived += (sender, e) => PropogateEvent(e, deviceClass, EnumUsbEvent.Inserted);
             insertWatcher.Start();
 
-            var removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            var removeQuery = new WqlEventQuery($"SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA '{deviceClass}'");
             ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
-            removeWatcher.EventArrived += (sender, e) => PropogateEvent(e, EnumUsbEvent.Removed);
+            removeWatcher.EventArrived += (sender, e) => PropogateEvent(e, deviceClass, EnumUsbEvent.Removed);
             removeWatcher.Start();
         }
 
-        void PropogateEvent(EventArrivedEventArgs e, EnumUsbEvent eventType)
+        void PropogateEvent(EventArrivedEventArgs e, string deviceClass, EnumUsbEvent eventType)
         {
             ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
 
             var deviceID = instance["DeviceID"];
             if (deviceID != null)
             {
-                var device = new USBDevice($"{deviceID}");
+                var device = new USBDevice($"{deviceClass} {deviceID}");
 
                 OnUsbEvent(new UsbEventArgs(device, eventType));
             }
