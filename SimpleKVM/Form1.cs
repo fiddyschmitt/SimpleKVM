ad4c65e358160bf0767f7cb28df6b69b0f7eeca5
Fidel <15338956+fiddyschmitt@users.noreply.github.com>
2024-05-31 16:42:04 +1000

    Update to .NET 8

--- SimpleKVM/Form1.cs
+++ SimpleKVM/Form1.cs
@@ -16,6 +16,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MethodInvoker = System.Windows.Forms.MethodInvoker;
 
 namespace SimpleKVM
 {
