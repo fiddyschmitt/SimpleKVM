fdaadd27bce80f8b3f9de3bc96c82c08e7c63868
Fidel <15338956+fiddyschmitt@users.noreply.github.com>
2024-06-06 23:09:24 +1000

    Add exception handler when monitor layout cannot be displayed

--- SimpleKVM/GUI/UcMonitorLayout.cs
+++ SimpleKVM/GUI/UcMonitorLayout.cs
@@ -24,78 +24,86 @@ namespace SimpleKVM.GUI
 
         private void UcMonitorLayout_Load(object sender, EventArgs e)
         {
-            var left = Screen.AllScreens.Min(screen => screen.Bounds.Left);
-            var right = Screen.AllScreens.Max(screen => screen.Bounds.Right);
-            var top = Screen.AllScreens.Min(screen => screen.Bounds.Top);
-            var bottom = Screen.AllScreens.Max(screen => screen.Bounds.Bottom);
+            try
+            {
+                throw new Exception("blah");
+                var left = Screen.AllScreens.Min(screen => screen.Bounds.Left);
+                var right = Screen.AllScreens.Max(screen => screen.Bounds.Right);
+                var top = Screen.AllScreens.Min(screen => screen.Bounds.Top);
+                var bottom = Screen.AllScreens.Max(screen => screen.Bounds.Bottom);
 
-            var virtualScreen = new Rectangle(left, top, right - left, bottom - top);
-            var offset = new Point(-virtualScreen.X, -virtualScreen.Y);
-            virtualScreen.Offset(offset);
+                var virtualScreen = new Rectangle(left, top, right - left, bottom - top);
+                var offset = new Point(-virtualScreen.X, -virtualScreen.Y);
+                virtualScreen.Offset(offset);
 
-            //gdiDrawer1.BackColor = Color.FromArgb(23, 23, 23);    //dark grey
+                //gdiDrawer1.BackColor = Color.FromArgb(23, 23, 23);    //dark grey
 
-            var scaleX = (monitorDrawer.Width - 100) / (double)virtualScreen.Width;
-            var scaleY = (monitorDrawer.Height - 100) / (double)virtualScreen.Height;
-            var scale = Math.Min(scaleX, scaleY);
+                var scaleX = (monitorDrawer.Width - 100) / (double)virtualScreen.Width;
+                var scaleY = (monitorDrawer.Height - 100) / (double)virtualScreen.Height;
+                var scale = Math.Min(scaleX, scaleY);
 
-            var drawRects = Screen.AllScreens
-                                    .Select(screen =>
-                                    {
-                                        var screenRect = screen.Bounds;
-                                        screenRect.Offset(offset);
-
-                                        var drawRect = new Rectangle(
-                                            (int)(screenRect.X * scale),
-                                            (int)(screenRect.Y * scale),
-                                            (int)(screenRect.Width * scale),
-                                            (int)(screenRect.Height * scale));
-
-                                        return new
+                var drawRects = Screen.AllScreens
+                                        .Select(screen =>
                                         {
-                                            ScreenName = screen.DeviceName,
-                                            ScreenIndex = screen.ScreenIndex(),
-                                            DrawRectable = drawRect
-                                        };
-                                    })
-                                    .OrderBy(screen => screen.ScreenIndex)
-                                    .ToList();
+                                            var screenRect = screen.Bounds;
+                                            screenRect.Offset(offset);
 
-            var rightMost = drawRects.Max(rect => rect.DrawRectable.Right);
-            var bottomMost = drawRects.Max(rect => rect.DrawRectable.Bottom);
-            var shiftRight = (int)((monitorDrawer.Width - rightMost) / 2d);
-            var shiftDown = (int)((monitorDrawer.Height - bottomMost) / 2d);
-            var offsetToCenter = new Point(shiftRight, shiftDown);
+                                            var drawRect = new Rectangle(
+                                                (int)(screenRect.X * scale),
+                                                (int)(screenRect.Y * scale),
+                                                (int)(screenRect.Width * scale),
+                                                (int)(screenRect.Height * scale));
+
+                                            return new
+                                            {
+                                                ScreenName = screen.DeviceName,
+                                                ScreenIndex = screen.ScreenIndex(),
+                                                DrawRectable = drawRect
+                                            };
+                                        })
+                                        .OrderBy(screen => screen.ScreenIndex)
+                                        .ToList();
+
+                var rightMost = drawRects.Max(rect => rect.DrawRectable.Right);
+                var bottomMost = drawRects.Max(rect => rect.DrawRectable.Bottom);
+                var shiftRight = (int)((monitorDrawer.Width - rightMost) / 2d);
+                var shiftDown = (int)((monitorDrawer.Height - bottomMost) / 2d);
+                var offsetToCenter = new Point(shiftRight, shiftDown);
 
 
-            var borderColour = Color.FromArgb(209, 209, 209);   //light gray
-            borderColour = Color.Black;
-            var borderPen = new Pen(borderColour, 1);
+                var borderColour = Color.FromArgb(209, 209, 209);   //light gray
+                borderColour = Color.Black;
+                var borderPen = new Pen(borderColour, 1);
 
-            var fontFamily = new FontFamily("Arial");
-            var font = new Font(fontFamily, (int)(360 * scale), FontStyle.Regular, GraphicsUnit.Point);
-            var textBrush = new SolidBrush(borderColour);
+                var fontFamily = new FontFamily("Arial");
+                var font = new Font(fontFamily, (int)(360 * scale), FontStyle.Regular, GraphicsUnit.Point);
+                var textBrush = new SolidBrush(borderColour);
 
-            monitors = drawRects
-                            .Select(rect =>
-                            {
-                                var drawRect = rect.DrawRectable;
-                                drawRect.Offset(offsetToCenter);
+                monitors = drawRects
+                                .Select(rect =>
+                                {
+                                    var drawRect = rect.DrawRectable;
+                                    drawRect.Offset(offsetToCenter);
 
-                                var r = new MonitorBox(rect.ScreenName, drawRect, borderPen, $"{rect.ScreenIndex}", EnumPosition.Center, font, textBrush);
-                                return r;
-                            })
-                            .OfType<MonitorBox>()
-                            .ToList();
+                                    var r = new MonitorBox(rect.ScreenName, drawRect, borderPen, $"{rect.ScreenIndex}", EnumPosition.Center, font, textBrush);
+                                    return r;
+                                })
+                                .OfType<MonitorBox>()
+                                .ToList();
 
-            var elems = monitors
-                            .OfType<Element>()
-                            .ToList();
+                var elems = monitors
+                                .OfType<Element>()
+                                .ToList();
 
-            monitorDrawer.AllowDragging = false;
-            monitorDrawer.Draw(elems);
+                monitorDrawer.AllowDragging = false;
+                monitorDrawer.Draw(elems);
 
-            monitorDrawer.BoxClicked += BoxClicked;
+                monitorDrawer.BoxClicked += BoxClicked;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error displaying monitor layout", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         public void SelectMonitor(string monitorUniqueId)
