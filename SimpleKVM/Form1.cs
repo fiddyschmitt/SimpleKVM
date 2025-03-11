b1796cdc4e546aa4fdb710de14572ab7ecd70bf7
Fidel <15338956+fiddyschmitt@users.noreply.github.com>
2025-03-11 18:19:54 +1000

    Increment version number

--- SimpleKVM/Form1.cs
+++ SimpleKVM/Form1.cs
@@ -28,7 +28,7 @@ namespace SimpleKVM
         readonly USBSystem? usbSystem;
         ListViewEx<Rule>? ruleListview;
         const string ProgramName = "Simple KVM";
-        const string Version = "2.1.0";
+        const string Version = "2.2.0";
         public static List<Rule> Rules { get; protected set; } = [];
         static readonly string SettingsFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "rules.json");
         static readonly string ConfigFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
