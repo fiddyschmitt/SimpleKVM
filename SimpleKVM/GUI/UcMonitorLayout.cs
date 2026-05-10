fe93ee195096a0b8354da36e696f7d016930f6e9
Fidel <15338956+fiddyschmitt@users.noreply.github.com>
2026-05-10 21:00:23 +1000

    Remove inconsistent DPI scaling

--- SimpleKVM/GUI/UcMonitorLayout.cs
+++ SimpleKVM/GUI/UcMonitorLayout.cs
@@ -1,5 +1,4 @@
 ﻿using SimpleKVM.GUI.Drawing.Elements;
-using SimpleKVM.Utilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -48,43 +47,25 @@ namespace SimpleKVM.GUI
                 var top = Screen.AllScreens.Min(screen => screen.Bounds.Top);
                 var bottom = Screen.AllScreens.Max(screen => screen.Bounds.Bottom);
 
-                var allScreensHaveSameScale = Screen.AllScreens
-                                                .Select(screen =>
-                                                {
-                                                    (var screenScaleX, var screenScaleY) = User32Utils.GetScalingFactor(screen);
-                                                    return $"{screenScaleX},{screenScaleY}";
-                                                })
-                                                .Distinct()
-                                                .Count() == 1;
-
                 var virtualScreen = new Rectangle(left, top, right - left, bottom - top);
                 var offset = new Point(-virtualScreen.X, -virtualScreen.Y);
                 virtualScreen.Offset(offset);
 
-                //gdiDrawer1.BackColor = Color.FromArgb(23, 23, 23);    //dark grey
-
                 var scaleX = (monitorDrawer.Width - 100) / (double)virtualScreen.Width;
                 var scaleY = (monitorDrawer.Height - 100) / (double)virtualScreen.Height;
                 var scale = Math.Min(scaleX, scaleY);
-                var applicationScale = DeviceDpi / 96f;
-                if (allScreensHaveSameScale || applicationScale == 0)
-                {
-                    applicationScale = 1;
-                }
 
                 var drawRects = Screen.AllScreens
                                         .Select(screen =>
                                         {
-                                            (var screenScaleX, var screenScaleY) = User32Utils.GetScalingFactor(screen);
-
                                             var screenRect = screen.Bounds;
                                             screenRect.Offset(offset);
 
                                             var drawRect = new Rectangle(
-                                                (int)(screenRect.X * scale / applicationScale),
-                                                (int)(screenRect.Y * scale / applicationScale),
-                                                (int)(screenRect.Width * scale / screenScaleX),
-                                                (int)(screenRect.Height * scale / screenScaleY));
+                                                (int)(screenRect.X * scale),
+                                                (int)(screenRect.Y * scale),
+                                                (int)(screenRect.Width * scale),
+                                                (int)(screenRect.Height * scale));
 
                                             return new
                                             {
